b9ca46e0dd2c3f20c21573efaab2a8512f97d7ed
Tean Lai <laitean19@gmail.com>
2025-05-14 21:52:02 -0400

    function calls

--- main.py
+++ main.py
@@ -38,6 +38,18 @@ class Mov(Instruction):
 
 
 @dataclass
+class Push(Instruction):
+    t: str
+    reg: str
+
+
+@dataclass
+class Pop(Instruction):
+    t: str
+    reg: str
+
+
+@dataclass
 class Ret(Instruction):
     pass
 
@@ -78,23 +90,8 @@ class Cmp(Operator):
 
 
 @dataclass
-class Setl(Operator):
-    pass
-
-
-@dataclass
-class Setg(Operator):
-    pass
-
-
-@dataclass
-class Setle(Operator):
-    pass
-
-
-@dataclass
-class Setge(Operator):
-    pass
+class Set(Operator):
+    code: str
 
 
 @dataclass
@@ -182,27 +179,21 @@ def format_operand(operand: Operand) -> str:
 def format_operator(operator):
     match operator:
         case Neg():
-            return "negl"
+            return "negq"
         case Not():
-            return "notl"
+            return "notq"
         case Add():
-            return "addl"
+            return "addq"
         case Sub():
-            return "subl"
+            return "subq"
         case Mul():
-            return "imull"
+            return "imulq"
         case Cmp():
-            return "cmpl"
-        case Setl():
-            return "setl"
-        case Setg():
-            return "setg"
-        case Setle():
-            return "setle"
-        case Setge():
-            return "setge"
+            return "cmpq"
+        case Set(code):
+            return f"set{code}"
         case Test():
-            return "testl"
+            return "testq"
 
 
 def format_instruction(construct):
@@ -210,8 +201,13 @@ def format_instruction(construct):
     match construct:
         case Mov(t, src, dest):
             return [f"mov{t} {src}, {dest}"]
+        case Push(t, reg):
+            return [f"push{t} {reg}"]
+        case Pop(t, reg):
+            return [f"pop{t} {reg}"]
         case Ret():
-            return ["movq %rbp, %rsp", "popq %rbp", "retq"]
+            # return ["movq %rbp, %rsp", "popq %rbp", "retq"]
+            return ["retq"]
         case Unary(operator, operand):
             return [f"{format_operator(operator)} {operand}"]
         case Binary(operator, src, dest):
@@ -219,13 +215,16 @@ def format_instruction(construct):
         case AllocateStack(num):
             return [f"subq ${num}, %rsp"]
         case Call(t, name):
-            return [f"call{t} {name}"]
+            return [f"call{t} _{name}"]
         case Label(name):
-            return [f"_{name}:"]
+            name = "_" + name.replace(".", "_")
+            return [f"{name}:"]
         case Jump(label):
-            return [f"jmp _{label}"]
+            label = "_" + label.replace(".", "_")
+            return [f"jmp {label}"]
         case JumpCond(cond_code, label):
-            return [f"j{cond_code} _{label}"]
+            label = "_" + label.replace(".", "_")
+            return [f"j{cond_code} {label}"]
 
     raise NotImplementedError(f"Unknown instruction: {construct}")
 
@@ -242,14 +241,12 @@ def format_function(f: Function) -> list[str]:
         f".globl _{f.name}",
         ".p2align	4, 0x90",
         f"_{f.name}:",
-        "pushq %rbp",
-        "movq %rsp, %rbp",
     ]
     for instruction in f.instructions:
         output.extend(format_instruction(instruction))
 
     output.append("movq %rbp, %rsp")
-    output.append("xorl %eax, %eax")
+    output.append("xorq %rax, %rax")
     output.append("popq %rbp")
     output.append("retq")
 
@@ -281,26 +278,73 @@ def format_program(prog: Program):
     return output
 
 
+def fake_main_to_assembly(func):
+    lines = []
+    lines.append(Push("q", "%rbp"))
+    lines.append(Mov("q", "%rsp", "%rbp"))
+
+    if "args" in func and len(func["args"]) > 0:
+
+        arg_regs = ["%rdi", "%rsi", "%rdx", "%rcx", "%r8", "%r9"]
+
+        if func.get("args", []):
+            args = func["args"]
+            var_count = len(args)
+
+            stack_bytes = (var_count + 1) * 8
+            if stack_bytes > 0:
+                lines.append(AllocateStack(stack_bytes))
+                lines.append(Mov("q", "%rbx", f"{var_count * 8}(%rsp)"))
+                lines.append(Mov("q", "%rsi", "%rbx"))
+
+            for i, arg in enumerate(args):
+                name = arg["name"]
+
+                offset = 8 * i
+                lines.append(Mov("q", "%rbx", "%rdi"))
+                lines.append(Mov("q", f"${i + 1}", "%rsi"))
+                if arg["type"] == "int":
+                    lines.append(Call("q", "_bril_parse_int"))
+                else:
+                    lines.append(Call("q", "_bril_parse_bool"))
+                lines.append(Mov("q", "%rax", f"{offset}(%rsp)"))
+
+            for i, arg in enumerate(args):
+                off = i * 8
+                if i < len(arg_regs):
+                    lines.append(Mov("q", f"{off}(%rsp)", arg_regs[i]))
+                else:
+                    raise NotImplementedError(">6 args")
+            lines.append(Mov("q", f"{var_count * 8}(%rsp)", "%rbx"))
+
+    lines.append(Call("q", "main_main"))
+    # lines.append(Jump("main_main"))
+
+    lines.extend([Mov("q", "%rbp", "%rsp"), Pop("q", "%rbp"), Ret()])
+
+    return Function(func["name"], lines)
+
+
 def func_to_assembly(func):
 
     lines = []
+    lines.append(Push("q", "%rbp"))
+    lines.append(Mov("q", "%rsp", "%rbp"))
 
     var_types = {}
     var_slots = {}
     var_count = 0
 
-    if "args" in func and len(func["args"]) > 0:
-        var_slots[0] = var_count
-        offset = var_count * 8
-        var_count += 1
-
-        for i, arg in enumerate(func.get("args", [])):
-            if debug_mode:
-                print("arg:", arg)
-            name = arg["name"]
-            var_types[name] = arg["type"]
-            var_slots[name] = var_count
+    arg_regs = ["%rdi", "%rsi", "%rdx", "%rcx", "%r8", "%r9"]
+    for i, arg in enumerate(func.get("args", [])):
+        if i < len(arg_regs):
+            var_slots[arg["name"]] = var_count
+            var_types[arg["name"]] = arg["type"]
+            assert var_count == i
             var_count += 1
+            # lines.append(Mov("q", arg_regs[i], f"{off}(%rsp)"))
+        else:
+            raise NotImplementedError(">6")
 
     for instr in func["instrs"]:
         if "dest" not in instr:
@@ -321,28 +365,12 @@ def func_to_assembly(func):
     if stack_bytes > 0:
         lines.append(AllocateStack(stack_bytes))
 
-    if 0 in var_slots:
-        offset = var_slots[0] * 8
-        assert offset == 0
-        lines.append(Mov("q", "%rsi", f"{offset}(%rsp)"))
-
     for i, arg in enumerate(func.get("args", [])):
-        assert 0 in var_slots
-
-        if debug_mode:
-            print(i, arg)
-        name = arg["name"]
-
-        offset = 8 * var_slots[name]
-
-        # lines.append(ParseCmdLine(f"{offset}(%ebx)", i, arg["type"]))
-        lines.append(Mov("q", "0(%rsp)", "%rdi"))
-        lines.append(Mov("q", f"${i + 1}", "%rsi"))
-        if var_types[name] == "int":
-            lines.append(Call("q", "__bril_parse_int"))
+        if i < len(arg_regs):
+            off = i * 8
+            lines.append(Mov("q", arg_regs[i], f"{off}(%rsp)"))
         else:
-            lines.append(Call("q", "__bril_parse_bool"))
-        lines.append(Mov("l", "%eax", f"{offset}(%rsp)"))
+            raise NotImplementedError(">6")
 
     for instr in func["instrs"]:
         if debug_mode:
@@ -363,27 +391,27 @@ def func_to_assembly(func):
                 off = var_slots[dest] * 8
 
                 if instr["type"] == "int":
-                    lines.append(Mov("l", f"${val}", f"{off}(%rsp)"))
+                    lines.append(Mov("q", f"${val}", f"{off}(%rsp)"))
                 else:
                     if val:
-                        lines.append(Mov("l", f"$1", f"{off}(%rsp)"))
+                        lines.append(Mov("q", f"$1", f"{off}(%rsp)"))
                     else:
-                        lines.append(Mov("l", f"$0", f"{off}(%rsp)"))
+                        lines.append(Mov("q", f"$0", f"{off}(%rsp)"))
 
             elif op in ("add", "sub", "mul"):
                 arg1, arg2 = instr["args"]
                 dest = instr["dest"]
                 off1 = var_slots[arg1] * 8
                 off2 = var_slots[arg2] * 8
-                lines.append(Mov("q", f"{off1}(%rsp)", "%eax"))
+                lines.append(Mov("q", f"{off1}(%rsp)", "%rax"))
 
                 temp = {"add": Add(), "sub": Sub(), "mul": Mul()}
-                lines.append(Binary(temp[op], f"{off2}(%rsp)", "%eax"))
+                lines.append(Binary(temp[op], f"{off2}(%rsp)", "%rax"))
 
                 off_dest = var_slots[dest] * 8
-                lines.append(Mov("l", "%eax", f"{off_dest}(%rsp)"))
+                lines.append(Mov("q", "%rax", f"{off_dest}(%rsp)"))
 
-            elif op in ("lt", "gt", "le", "ge"):
+            elif op in ("lt", "gt", "le", "ge", "eq"):
                 arg1, arg2 = instr["args"]
                 dest = instr["dest"]
                 off1 = var_slots[arg1] * 8
@@ -391,59 +419,60 @@ def func_to_assembly(func):
                 off_dest = var_slots[dest] * 8
 
                 cmp_map = {
-                    "lt": Setl(),
-                    "gt": Setg(),
-                    "le": Setle(),
-                    "ge": Setge(),
+                    "lt": "l",
+                    "gt": "g",
+                    "le": "le",
+                    "ge": "ge",
+                    "eq": "e",
                 }
 
-                lines.append(Mov("l", f"{off1}(%rsp)", "%eax"))
-                lines.append(Binary(Cmp(), f"{off2}(%rsp)", "%eax"))
-                lines.append(Unary(cmp_map[op], "%al"))
-                lines.append(Mov("zbl", "%al", "%eax"))
-                lines.append(Mov("l", "%eax", f"{off_dest}(%rsp)"))
+                lines.append(Mov("q", f"{off1}(%rsp)", "%rax"))
+                lines.append(Binary(Cmp(), f"{off2}(%rsp)", "%rax"))
+                lines.append(Unary(Set(cmp_map[op]), "%al"))
+                lines.append(Mov("zbq", "%al", "%rax"))
+                lines.append(Mov("q", "%rax", f"{off_dest}(%rsp)"))
 
             elif op == "ret":
-                if len(instr["args"]) > 0:
+                if "args" in instr and len(instr["args"]) > 0:
                     ret_var = instr["args"][0]
                     off = var_slots[ret_var] * 8
-                    lines.append(Mov("q", f"{off}(%rsp)", "%eax"))
-                lines.append(Ret())
+                    lines.append(Mov("q", f"{off}(%rsp)", "%rax"))
+                lines.extend([Mov("q", "%rbp", "%rsp"), Pop("q", "%rbp"), Ret()])
 
             elif op == "print":
                 args = [f"{8 * var_slots[x]}(%rsp)" for x in instr["args"]]
                 types = [var_types[x] for x in instr["args"]]
 
-                # lines.append(Print(args, types))
-
                 n = len(args)
 
                 for i in range(n):
-                    lines.append(Mov("l", args[i], "%edi"))
+                    lines.append(Mov("q", args[i], "%rdi"))
                     if types[i] == "int":
-                        lines.append(Call("q", "__bril_print_int"))
+                        lines.append(Call("q", "_bril_print_int"))
                     else:
-                        lines.append(Call("q", "__bril_print_bool"))
+                        lines.append(Call("q", "_bril_print_bool"))
 
                     if i < n - 1:
-                        lines.append(Call("q", "__bril_print_sep"))
+                        lines.append(Call("q", "_bril_print_sep"))
                     else:
-                        lines.append(Call("q", "__bril_print_end"))
+                        lines.append(Call("q", "_bril_print_end"))
 
             elif op == "id":
                 dest = instr["dest"]
                 src = instr["args"][0]
                 off_src = var_slots[src] * 8
                 off_dest = var_slots[dest] * 8
-                lines.append(Mov("l", f"{off_src}(%rsp)", f"{off_dest}(%rsp)"))
+
+                lines.append(Mov("q", f"{off_src}(%rsp)", "%rax"))
+                lines.append(Mov("q", "%rax", f"{off_dest}(%rsp)"))
 
             elif op == "br":
                 true_label, false_label = instr["labels"]
                 cond_var = instr["args"][0]
                 off = var_slots[cond_var] * 8
 
-                lines.append(Mov("l", f"{off}(%rsp)", "%eax"))
-                lines.append(Binary(Test(), "%eax", "%eax"))
+                lines.append(Mov("q", f"{off}(%rsp)", "%rax"))
+                lines.append(Binary(Test(), "%rax", "%rax"))
                 lines.append(JumpCond("ne", true_label))
                 lines.append(Jump(false_label))
 
@@ -451,14 +480,46 @@ def func_to_assembly(func):
                 target = instr["labels"][0]
                 lines.append(Jump(target))
 
+            elif op == "call":
+                # print(instr)
+                func_name = instr["funcs"][0]
+                args = instr.get("args", [])
+                dest = instr.get("dest", None)
+
+                for i, a in enumerate(args):
+                    off = var_slots[a] * 8
+                    if i < len(arg_regs):
+                        lines.append(Mov("q", f"{off}(%rsp)", arg_regs[i]))
+                    else:
+                        raise NotImplementedError(">6 args")
+
+                lines.append(Call("q", func_name))
+
+                if dest is not None:
+                    off_d = var_slots[dest] * 8
+                    lines.append(Mov("q", "%rax", f"{off_d}(%rsp)"))
+
             else:
                 raise NotImplementedError(f"not supported op: {op}")
+
+    lines.extend([Mov("q", "%rbp", "%rsp"), Pop("q", "%rbp"), Ret()])
 
     return Function(func["name"], lines)
 
 
 def bril_to_assembly(prog):
-    return Program([func_to_assembly(func) for func in prog["functions"]])
+    functions = []
+    for func in prog["functions"]:
+        name = func["name"]
+        if name == "main":
+            functions.append(fake_main_to_assembly(func))
+            func["name"] = "main_main"
+            functions.append(func_to_assembly(func))
+        else:
+            functions.append(func_to_assembly(func))
+        # functions.append(func_to_assembly(func))
+
+    return Program(functions)
 
 
 if __name__ == "__main__":
